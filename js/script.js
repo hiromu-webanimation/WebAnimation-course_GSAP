4a020919b0d4572356f7fb9d9869c2ed26c2b882
Hiromu-Chiba118 <108908014+Hiromu-Chiba118@users.noreply.github.com>
2025-12-16 21:49:28 +0900

    書き方修正（AIが）

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 jQuery(function ($) {
   // animation
   // parallax
+
+  ScrollTrigger.create({
+    start: 'top -100',
+    end: 'max',
+    toggleClass: {
+      targets: '.header',
+      className: 'is-scrolled',
+    },
+  });
 });
