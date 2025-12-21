af008a3cb5c23e35f41c32bbbf826a089bc418b9
Hiromu-Chiba118 <108908014+Hiromu-Chiba118@users.noreply.github.com>
2025-12-21 11:22:12 +0900

    動き調整

--- js/script.js
+++ js/script.js
@@ -1,14 +1,144 @@
 // animation======================
 // ローディング〜mvアニメーション
+gsap.set('.header , .fv__nav', {
+  opacity: 0,
+});
 
+const opening = gsap.timeline();
 
+opening
+  .fromTo(
+    '.loader__logo-img',
+    {
+      yPercent: 100,
+      opacity: 0,
+    },
+    {
+      yPercent: 0,
+      opacity: 1,
+      duration: 1,
+      ease: 'power2.inOut',
+    }
+  )
+  .to(
+    '.loader__logo-img',
+    {
+      yPercent: -100,
+      duration: 1,
+      opacity: 0,
+      ease: 'power2.inOut',
+    },
+    '+=0.3'
+  )
+  .to(
+    '.loader',
+    {
+      clipPath: 'inset(0% 0% 100% 0% )',
+      duration: 1,
+      ease: 'power2.inOut',
+    },
+    '<'
+  )
+  .to(
+    '.loader',
+    {
+      display: 'none',
+    },
+    '>'
+  )
+  .fromTo(
+    '.fv__bg',
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+      ease: 'power2.inOut',
+    },
+    '-=0.8'
+  )
+  .fromTo(
+    '.fv__title-line-span',
+    {
+      yPercent: 100,
+      opacity: 0,
+    },
+    {
+      yPercent: 0,
+      opacity: 1,
+      duration: 1.3,
+      ease: 'power2.inOut',
+    },
+    '<'
+  )
+  .to(
+    '.fv__nav , .header',
+    {
+      opacity: 1,
+      duration: 1,
+      ease: 'power2.inOut',
+    },
+    '-=0.2'
+  );
 
+// 汎用fadeIn
+let fadeInElements = document.querySelectorAll('.js-fade-in');
+fadeInElements.forEach((fadeInElement) => {
+  gsap.fromTo(
+    fadeInElement,
+    {
+      opacity: 0,
+      y: 5,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.8,
+      ease: 'power2.inOut',
 
-// header is-scrolled
+      scrollTrigger: {
+        trigger: fadeInElement,
+        start: 'top 90%',
+        toggleClass: {
+          targets: fadeInElement,
+          className: 'is-active',
+        },
+      },
+    }
+  );
+});
+
+// セクションタイトル
+let sectionHeaders = document.querySelectorAll('.section-header');
+sectionHeaders.forEach((sectionHeader) => {
+  gsap.fromTo(
+    sectionHeader,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 0.8,
+      ease: 'power2.inOut',
+      scrollTrigger: {
+        trigger: sectionHeader,
+        start: 'top 90%',
+        toggleClass: {
+          targets: sectionHeader,
+          className: 'is-visible',
+        },
+        once: true,
+      },
+    }
+  );
+});
+
+// header スクロールで変化
 gsap.to('.header', {
   scrollTrigger: {
     trigger: 'body',
-    start: 'top -100',
+    start: 'top -200',
     end: 'max',
     toggleClass: {
       targets: '.header',
@@ -74,33 +204,6 @@ staggerElements.forEach((staggerElement) => {
       scrollTrigger: {
         trigger: staggerElement,
         start: 'top 90%',
-      },
-    }
-  );
-});
-
-// 汎用fadeIn
-let fadeInElements = document.querySelectorAll('.js-fade-in');
-fadeInElements.forEach((fadeInElement) => {
-  gsap.fromTo(
-    fadeInElement,
-    {
-      opacity: 0,
-      y: 5,
-    },
-    {
-      opacity: 1,
-      y: 0,
-      duration: 0.8,
-      ease: 'power2.inOut',
-
-      scrollTrigger: {
-        trigger: fadeInElement,
-        start: 'top 90%',
-        toggleClass: {
-          targets: fadeInElement,
-          className: 'is-active',
-        },
       },
     }
   );
